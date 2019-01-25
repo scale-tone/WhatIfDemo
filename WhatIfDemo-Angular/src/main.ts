7e41e9db3704168dc38c0e27611b2d79ae3d8c16
Konstantin Lepeshenkov <konstantin.lepeshenkov@gmail.com>
2019-01-25 23:36:12 +0100

    minor fixes

--- WhatIfDemo-Angular/src/main.ts
+++ WhatIfDemo-Angular/src/main.ts
@@ -10,10 +10,3 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-  
-function checkLoginState(param){
-
-	console.log('checkLoginState!');
-
-}
